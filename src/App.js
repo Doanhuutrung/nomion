9ef21737c260ca89a7237f387ddbd4a6b1baba4a
Huu Trung <huutrungdoan22202@gmail.com>
2023-12-20 17:29:28 +0700

    remove some unused files and create some component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+
+import "./dist/App.css";
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
+        <MainContent/>
         <Footer />
       </div>
     </Router>
---
+++ src/Components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent() {
+  return <main className="main min-h-[100svh]"> 
+    
+  </main>;
+}
+
+export default MainContent;
+
+//Small viewport units = svh
