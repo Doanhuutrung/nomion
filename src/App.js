ae6609b81c98b88462953c75b14e0efd5b5ad754
Huu Trung <huutrungdoan22202@gmail.com>
2023-12-20 14:32:11 +0700

    test tailwind library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
   return (
     <header>
+      <div class="text-3xl text-amber-400	"> Đây là header </div>
     </header>
   );
 }
