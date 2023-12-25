b7b4f42238f19df8f0ad9719f2f3bb6a39978300
Huu Trung <huutrungdoan22202@gmail.com>
2023-12-25 22:38:49 +0700

    [upd] gallery product

--- src/Components/sections/Gallery.jsx
+++ src/Components/sections/Gallery.jsx
@@ -4,7 +4,7 @@ const Gallery = () => {
       <div className="m-auto flex max-w-[1440px] flex-col flex-wrap lg:flex-row">
         <div class="mb-[50px] flex h-full justify-center px-[24px] lg:w-1/2">
           <div class="relative flex flex-col items-center justify-center">
-            <div class="rounded-full bg-gray-600 text-white absolute top-2 z-10 w-fit md:top-6">
+            <div class="absolute top-2 z-10 w-fit rounded-full bg-gray-600 text-white md:top-6">
               <p class="px-[20px] py-[4px] text-center text-base font-semibold md:text-xl">
                 Ortho
               </p>
@@ -14,7 +14,7 @@ const Gallery = () => {
               alt="general.ortho"
               class="undefined h-full w-full rounded-lg object-cover md:max-w-lg"
             />
-            <div class="bg-gray-700	 flex w-full flex-col gap-3 rounded-lg p-4 transition-all duration-300 hover:opacity-100 md:absolute md:bottom-2 md:z-10 md:h-fit md:w-[95%] md:opacity-0">
+            <div class="flex	 w-full flex-col gap-3 rounded-lg bg-gray-700 p-4 transition-all duration-300 hover:opacity-100 md:absolute md:bottom-2 md:z-10 md:h-fit md:w-[95%] md:opacity-0">
               <p class="font-medium text-white">Thương hiệu thời trang số</p>
               <p class="h-5/6 text-[#BFBFBF] md:h-full">
                 Ortho sử dụng giải pháp Nomion trong việc định danh số 300 chiếc
@@ -29,13 +29,117 @@ const Gallery = () => {
                     class="text-sm font-semibold text-white"
                     href="https://ortho.fashion"
                   >
-                    Thư viện số 
+                    Thư viện số
                   </a>
                 </div>
               </div>
             </div>
           </div>
-        </div>   
+        </div>
+        <div class="mb-[50px] flex h-full justify-center px-[24px] lg:w-1/2">
+          <div class="relative flex flex-col items-center justify-center">
+            <div class="absolute top-2 z-10 w-fit rounded-full bg-gray-600 text-white md:top-6">
+              <p class="px-[20px] py-[4px] text-center text-base font-semibold md:text-xl">
+                Le J' Cafe
+              </p>
+            </div>
+            <img
+              src="https://assets.nomion.io/nomion-website/lej-banner.png"
+              alt="Le J' Cafe"
+              class="undefined h-full w-full rounded-lg object-cover md:max-w-lg"
+            />
+            <div class="flex	 w-full flex-col gap-3 rounded-lg bg-gray-700 p-4 transition-all duration-300 hover:opacity-100 md:absolute md:bottom-2 md:z-10 md:h-fit md:w-[95%] md:opacity-0">
+              <p class="font-medium text-white">Cafe đặc sản Đà Lạt</p>
+              <p class="h-5/6 text-[#BFBFBF] md:h-full">
+                Nomion cung cấp trình duyệt web đơn giản với nội dung tương tác
+                đa dạng được cá nhân hóa, giúp Le J' dễ dàng lan tỏa niềm đam mê
+                và tâm huyết với cà phê Việt.
+              </p>
+              <div class="flex justify-center gap-3 md:justify-start">
+                <div class="flex items-center rounded-lg border px-2 py-2 md:gap-2 md:px-4">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    class="text-sm font-semibold text-white"
+                    href="https://nomion.io/lej"
+                  >
+                    Thư viện số
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mb-[50px] flex h-full justify-center px-[24px] lg:w-1/2">
+          <div class="relative flex flex-col items-center justify-center">
+            <div class="absolute top-2 z-10 w-fit rounded-full bg-gray-600 text-white md:top-6">
+              <p class="px-[20px] py-[4px] text-center text-base font-semibold md:text-xl">
+                Đá non nước 2023
+              </p>
+            </div>
+            <img
+              src="https://assets.nomion.io/nomion-website/da-non-nuoc-banner.png"
+              alt="general.danonnuoc"
+              class="undefined h-full w-full rounded-lg object-cover md:max-w-lg"
+            />
+            <div class="flex	 w-full flex-col gap-3 rounded-lg bg-gray-700 p-4 transition-all duration-300 hover:opacity-100 md:absolute md:bottom-2 md:z-10 md:h-fit md:w-[95%] md:opacity-0">
+              <p class="font-medium text-white">Đá non nước</p>
+              <p class="h-5/6 text-[#BFBFBF] md:h-full">
+                Với giải pháp Nomion, 10 tác phẩm tham gia Hội thi điêu khắc đá
+                mỹ nghệ Non Nước 2023 lần đầu tiên được định danh số, chứng thực
+                độc bản và triển lãm trên không gian số.
+              </p>
+              <div class="flex justify-center gap-3 md:justify-start">
+                <div class="flex items-center rounded-lg border px-2 py-2 md:gap-2 md:px-4">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    class="text-sm font-semibold text-white"
+                    href="https://nomion.io/danonnuoc2023"
+                  >
+                    Thư viện số
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mb-[50px] flex h-full justify-center px-[24px] lg:w-1/2">
+          <div class="relative flex flex-col items-center justify-center">
+            <div class="absolute top-2 z-10 w-fit rounded-full bg-gray-600 text-white md:top-6">
+              <p class="px-[20px] py-[4px] text-center text-base font-semibold md:text-xl">
+                ELLE Fashion Show 2023
+              </p>
+            </div>
+            <img
+              src="https://storage.googleapis.com/assets-fygito/elle/nomion-gallery/image%20207.png"
+              alt="ELLE Fashion Show 2023"
+              class="undefined h-full w-full rounded-lg object-contain md:max-w-lg"
+            />
+            <div class="flex	 w-full flex-col gap-3 rounded-lg bg-gray-700 p-4 transition-all duration-300 hover:opacity-100 md:absolute md:bottom-2 md:z-10 md:h-fit md:w-[95%] md:opacity-0">
+              <p class="font-medium text-white">
+                ELLE Fashion Show 2023: Giao Lộ Thời Trang & Kiến Trúc
+              </p>
+              <p class="h-5/6 text-[#BFBFBF] md:h-full">
+                Chương trình trình diễn thời trang mang yếu tố liên ngành đầu
+                tiên được đồng tổ chức bởi ELLE Vietnam và ELLE Decoration
+                Vietnam.
+              </p>
+              <div class="flex justify-center gap-3 md:justify-start">
+                <div class="flex items-center rounded-lg border px-2 py-2 md:gap-2 md:px-4">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    class="text-sm font-semibold text-white"
+                    href="https://nomion.io/elleshow2023"
+                  >
+                    Thư viện số
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
